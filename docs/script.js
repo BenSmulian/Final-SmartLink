02f83fc3b6d9404330c83e4d0ae74f2d00da1134
BenSmulian <160271011+BenSmulian@users.noreply.github.com>
2024-04-01 21:59:02 +0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -5,7 +5,7 @@ function shortenURL() {
     var longUrl = document.getElementById('url').value.trim();
     
     // Make a request to your backend API to shorten the URL
-    fetch('https://main--earnest-yeot-04c6ff.netlify.app/', {
+    fetch('https://660b03be4be47e096df0a10a--visionary-stardust-52f7a4.netlify.app/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
