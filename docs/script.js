009dbe6ec3acb38988037e2e94b888c4c07702ba
BenSmulian <160271011+BenSmulian@users.noreply.github.com>
2024-04-01 20:16:12 +0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,14 +1,32 @@
-const http = require('http');
+// script.js (Frontend JavaScript)
+// Add your JavaScript code here
 
-const hostname = '0.0.0.0';
-const port = process.env.PORT || 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
-});
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+function shortenURL() {
+    var longUrl = document.getElementById('url').value.trim();
+    
+    // Make a request to your backend API to shorten the URL
+    fetch('https://660aeb95e4397f91746814f6--earnest-yeot-04c6ff.netlify.app/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ longUrl: longUrl })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        if (data.shortUrl) {
+            document.getElementById('shortenedUrl').innerText = 'Shortened URL: ' + data.shortUrl;
+        } else {
+            document.getElementById('shortenedUrl').innerText = 'Error: Unable to shorten URL.';
+        }
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        document.getElementById('shortenedUrl').innerText = 'Error: Unable to shorten URL.';
+    });
+}
