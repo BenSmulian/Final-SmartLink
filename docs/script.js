8b90d6f99efc68e8277c1b6fd72a3f707306cfa8
BenSmulian <160271011+BenSmulian@users.noreply.github.com>
2024-04-01 19:51:15 +0300

    Ruined script.js

--- docs/script.js
+++ docs/script.js
@@ -1,33 +1,14 @@
-// script.js (Frontend JavaScript)
-// Add your JavaScript code here
+const http = require('http');
 
-function shortenURL() {
-    var longUrl = document.getElementById('url').value.trim();
-    
-    // Make a request to your backend API to shorten the URL
-    fetch('https://[2a00:a041:21e1:a600::1000]:1337/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ longUrl: longUrl })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        if (data.shortUrl) {
-            document.getElementById('shortenedUrl').innerText = 'Shortened URL: ' + data.shortUrl;
-        } else {
-            document.getElementById('shortenedUrl').innerText = 'Error: Unable to shorten URL.';
-        }
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-        document.getElementById('shortenedUrl').innerText = 'Error: Unable to shorten URL.';
-    });
-}
+const hostname = '0.0.0.0';
+const port = process.env.PORT || 3000;
 
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
